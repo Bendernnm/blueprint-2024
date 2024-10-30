86366e6e56503e6378c6459542b2cc9bb73620a9
bender <bender.d1mon@gmail.com>
2024-10-30 21:56:42 +0200

    root route

--- src/http/api/index.ts
+++ src/http/api/index.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import * as core from "express-serve-static-core";
+import {rootRoute} from "./root";
 
 export default (): core.Express => {
     const app = express();
+
+    app.get('/', rootRoute);
 
     return app;
 };
