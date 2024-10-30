e624a09fa16cf838c8d053349329a8310bae86e9
bender <bender.d1mon@gmail.com>
2024-10-30 21:58:02 +0200

    fin start cmd

--- src/http/server.ts
+++ src/http/server.ts
@@ -3,7 +3,7 @@ import app from './api';
 import CONFIG from "../config";
 
 export default (): void => {
-    const server = http.createServer(app);
+    const server = http.createServer(app());
 
     server.listen(CONFIG.PORT, CONFIG.HOST, () => {
         console.log('Server is running on http://%s:%d', CONFIG.HOST, CONFIG.PORT);
